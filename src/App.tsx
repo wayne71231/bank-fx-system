5d516a689c5ebb2f3fff041073fa8060557bbb8c
邱竑瑋 <wayne@qiuhongweideMacBook-Air.local>
2025-11-26 16:11:46 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,1 @@
+export default function App(){ return (<div>Bank FX System</div>); }
---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTBsOSAR8B-Egce8WWLw_IFQSpKHY6Kjg",
+  authDomain: "bank-system-2a3f1.firebaseapp.com",
+  projectId: "bank-system-2a3f1",
+  storageBucket: "bank-system-2a3f1.firebasestorage.app",
+  messagingSenderId: "1096182651792",
+  appId: "1:1096182651792:web:a62e0c3a22b43d4ab67115",
+  measurementId: "G-R1EEKKJ4KL"
+};
